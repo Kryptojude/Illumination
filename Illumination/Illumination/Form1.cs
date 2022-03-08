55b79ea9123bddfde4f32b89d35bfcaea25a83e0
Kryptojude <68050323+Kryptojude@users.noreply.github.com>
2022-03-08 21:10:31 +0100

    light, vector class

---
+++ Illumination/Illumination/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Illumination
+{
+    public partial class Form1 : Form
+    {
+        Image ScreenBuffer;
+        Graphics ScreenBufferGfx;
+        Timer Timer;
+        public Form1()
+        {
+            InitializeComponent();
+            Paint += Form1_Paint;
+            ScreenBuffer = new Bitmap(500, 500);
+            ScreenBufferGfx = Graphics.FromImage(ScreenBuffer);
+            Timer = new Timer { Interval = 16, Enabled = true };
+            Timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.DrawImage(ScreenBuffer, 0, 0);
+        }
+    }
+}
---
+++ Illumination/Illumination/Light.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Illumination
+{
+    class Light
+    {
+        Point Location;
+        Point Direction_Unit_Vector;
+
+        public Light(Point start_location, Point start_direction_unit_vector)
+        {
+            Location = start_location;
+            Direction_Unit_Vector = start_direction_unit_vector;
+        }
+
+        private Point DistanceToVector(int distance)
+        {
+
+        }
+
+        public void Advance(int distance)
+        {
+            // Turn distance into (x,y)
+            Point add_vector = DistanceToVector(distance);
+            // Add vector onto Location
+            Point new_location = new Point();
+            Location.X += add_vector.X;
+            Location.Y += add_vector.Y;
+        }
+    }
+}
